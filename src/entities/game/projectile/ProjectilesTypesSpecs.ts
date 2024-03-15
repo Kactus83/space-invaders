763ea351e74f333dffa23351eaa1d5be569ac3ee
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-03-15 17:24:14 +0100

    Upgrade gameplay (projectiles)

--- src/entities/game/projectile/ProjectilesTypesSpecs.ts
+++ src/entities/game/projectile/ProjectilesTypesSpecs.ts
@@ -1,7 +1,7 @@
 import { ProjectileType } from './ProjectileType';
 
 export const ProjectileSpecs: Record<ProjectileType, { level: number, speed: number, damage: number }> = {
-    [ProjectileType.Basic]: { level: 1, speed: 5, damage: 10 },
-    [ProjectileType.Advanced]: { level: 2, speed: 7, damage: 15 },
-    [ProjectileType.Ultimate]: { level: 3, speed: 10, damage: 20 },
+    [ProjectileType.Basic]: { level: 1, speed: 250, damage: 10 },
+    [ProjectileType.Advanced]: { level: 2, speed: 350, damage: 15 },
+    [ProjectileType.Ultimate]: { level: 3, speed: 500, damage: 20 },
 };
