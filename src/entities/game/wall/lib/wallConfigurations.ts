0f8148dd99e149ff294de20dc6a068357cb0b9c1
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-03-19 12:02:45 +0100

    Debug wall creation

--- src/entities/game/wall/lib/wallConfigurations.ts
+++ src/entities/game/wall/lib/wallConfigurations.ts
@@ -18,26 +18,29 @@ export const wallTypeMapping: { [key: number]: WallType } = {
 
 // Configurations de vagues pour les murs
 export const wallWaveConfigurations = {
-  wave1: ["basic", "empty", "strong", "empty", "basic","basic", "empty", "strong", "empty", "basic","basic", "empty", "strong", "empty", "basic","basic", "empty", "strong", "empty", "basic"],
-  wave2: ["empty", "empty", "empty", "empty", "strong","strong", "basic", "empty", "basic", "strong","strong", "basic", "empty", "basic", "strong","strong", "empty", "empty", "empty", "empty"],
+  wave1: ["basic", "basic", "empty", "empty", "empty", "basic", "empty", "empty", "strong", "strong", "strong", "strong", "empty", "empty", "basic", "empty", "empty", "empty", "basic", "basic"],
+  wave2: ["basic", "basic", "empty", "empty", "empty", "basic", "empty", "empty", "strong", "empty", "empty", "strong", "empty", "empty", "basic", "empty", "empty", "empty", "basic", "basic"],
 };
 
 // Fonction pour assembler les mini-patterns en une configuration de vague complète
 export function assembleWallPattern(waveKey: string): number[][] {
-  const patternKeys = (wallWaveConfigurations as { [key: string]: string[] })[waveKey] || [];
+  const patternKeys = wallWaveConfigurations[waveKey as keyof typeof wallWaveConfigurations] || [];
   let wavePattern: number[][] = [];
 
-  patternKeys.forEach(key => {
-    const miniPattern = miniPatterns[key];
-    miniPattern.forEach((row, rowIndex) => {
-      if (!wavePattern[rowIndex]) {
-        wavePattern[rowIndex] = [];
-      }
-      row.forEach(typeIndex => {
-        wavePattern[rowIndex].push(...miniPattern[rowIndex]);
-      });
+  // Pour chaque ligne du mini-pattern (en assumant une hauteur uniforme pour tous)
+  for (let rowIndex = 0; rowIndex < 4; rowIndex++) {
+    wavePattern[rowIndex] = [];
+    
+    // Pour chaque mini-pattern spécifié dans la configuration de la vague
+    patternKeys.forEach(key => {
+      // Trouver le mini-pattern correspondant
+      const miniPattern = miniPatterns[key];
+      
+      // Ajouter la ligne courante du mini-pattern à la ligne courante de wavePattern
+      wavePattern[rowIndex] = wavePattern[rowIndex].concat(miniPattern[rowIndex]);
     });
-  });
+  }
 
   return wavePattern;
 }
+
