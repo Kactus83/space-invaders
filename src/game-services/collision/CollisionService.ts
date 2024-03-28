012ae513119747eac3e98bdc950926ba5e73bdb6
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-03-28 18:55:27 +0100

    Collision service optimized for performance

--- src/game-services/collision/CollisionService.ts
+++ src/game-services/collision/CollisionService.ts
@@ -1,44 +1,105 @@
 import { GameEntity } from "../../entities/GameEntity";
 import { GroundLine } from "../../entities/ground-line/GroundLine";
+import { Invader } from "../../entities/invader/Invader";
 import { Player } from "../../entities/player/Player";
+import { Projectile } from "../../entities/projectile/Projectile";
 import { Wall } from "../../entities/wall/Wall";
 import { ICollidable } from "./ICollidable";
 
 export class CollisionService {
-    private entities: GameEntity[] = [];
+    private players: Player[] = [];
+    private invaders: Invader[] = [];
+    private walls: Wall[] = [];
+    private projectiles: GameEntity[] = [];
+    private groundLines: GroundLine[] = [];
+
+    public registerEntity(entity: GameEntity): void {
+        if (entity instanceof Player) {
+            this.players.push(entity);
+        } else if (entity instanceof Invader) {
+            this.invaders.push(entity);
+        } else if (entity instanceof Wall) {
+            this.walls.push(entity);
+        } else if (entity instanceof Projectile) {
+            this.projectiles.push(entity);
+        } else if (entity instanceof GroundLine) {
+            this.groundLines.push(entity);
+        }
+    }
+
+    public unregisterEntity(entity: GameEntity): void {
+        let array = this.getArrayForEntity(entity);
+        const index = array.indexOf(entity);
+        if (index > -1) {
+            array.splice(index, 1);
+        }
+    }
 
     public checkCollisions(): void {
-        for (let i = 0; i < this.entities.length; i++) {
-            for (let j = i + 1; j < this.entities.length; j++) {
-                let entityA = this.entities[i];
-                let entityB = this.entities[j];
-
-                if (this.areColliding(entityA, entityB)) {
-                    entityA.onCollisionWith(entityB);
-                    entityB.onCollisionWith(entityA);
+        // Vérifications des collisions pour les projectiles
+        this.projectiles.forEach(projectile => {
+            // Collisions Projectile avec Invaders
+            this.invaders.forEach(invader => {
+                if (this.areColliding(projectile, invader)) {
+                    projectile.onCollisionWith(invader);
+                    invader.onCollisionWith(projectile);
                 }
-            }
-        }
-    }
-
-    private shouldCheckCollision(entityA: GameEntity, entityB: GameEntity): boolean {
-        // Évitez les collisions entre les murs
-        if (entityA instanceof Wall && entityB instanceof Wall) {
-            return false;
-        }
+            });
     
-        // Évitez les collisions entre le joueur et les murs/groundline
-        if (entityA instanceof Player && (entityB instanceof Wall || entityB instanceof GroundLine)) {
-            return false;
-        }
+            // Collision Projectile avec Player
+            this.players.forEach(player => {
+                if (this.areColliding(projectile, player)) {
+                    projectile.onCollisionWith(player);
+                    player.onCollisionWith(projectile);
+                }
+            });
     
-        if (entityB instanceof Player && (entityA instanceof Wall || entityA instanceof GroundLine)) {
-            return false;
-        }
+            // Collision Projectile avec Walls
+            this.walls.forEach(wall => {
+                if (this.areColliding(projectile, wall)) {
+                    projectile.onCollisionWith(wall);
+                    wall.onCollisionWith(projectile);
+                }
+            });
     
-        // Ajoutez d'autres conditions selon les besoins de votre jeu
-        return true;
-    }
+            // Collision Projectile avec GroundLine
+            this.groundLines.forEach(groundLine => {
+                if (this.areColliding(projectile, groundLine)) {
+                    projectile.onCollisionWith(groundLine);
+                    groundLine.onCollisionWith(projectile);
+                }
+            });
+        });
+    
+        // Vérifications des collisions pour les invaders
+        this.invaders.forEach(invader => {
+            // Collision Invader avec Player
+            this.players.forEach(player => {
+                if (this.areColliding(invader, player)) {
+                    invader.onCollisionWith(player);
+                    player.onCollisionWith(invader);
+                }
+            });
+    
+            // Collision Invader avec Walls
+            this.walls.forEach(wall => {
+                if (this.areColliding(invader, wall)) {
+                    invader.onCollisionWith(wall);
+                    wall.onCollisionWith(invader);
+                }
+            });
+    
+            // Collision Invader avec GroundLine
+            this.groundLines.forEach(groundLine => {
+                if (this.areColliding(invader, groundLine)) {
+                    invader.onCollisionWith(groundLine);
+                    groundLine.onCollisionWith(invader);
+                }
+            });
+        });
+    
+        // Note: Vous pouvez ajuster et ajouter d'autres types de vérifications si nécessaire
+    }    
 
     private areColliding(entityA: ICollidable, entityB: ICollidable): boolean {
         const boundsA = entityA.getCollisionBounds();
@@ -50,16 +111,19 @@ export class CollisionService {
                  boundsB.y + boundsB.height < boundsA.y);
     }
 
-    public registerEntity(entity: GameEntity): void {
-        if (!this.entities.includes(entity)) {
-            this.entities.push(entity);
+    private getArrayForEntity(entity: GameEntity): GameEntity[] {
+        if (entity instanceof Player) {
+            return this.players;
+        } else if (entity instanceof Invader) {
+            return this.invaders;
+        } else if (entity instanceof Wall) {
+            return this.walls;
+        } else if (entity instanceof Projectile) {
+            return this.projectiles;
+        } else if (entity instanceof GroundLine) {
+            return this.groundLines;
         }
+        throw new Error("Unknown entity type");
     }
-    
-    public unregisterEntity(entity: GameEntity): void {
-        const index = this.entities.indexOf(entity);
-        if (index > -1) {
-            this.entities.splice(index, 1);
-        }
-    }    
 }
+
