2098e40d7f0389a4448640d3b2aa58f336106ded
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-03-28 19:10:35 +0100

    modify ground line levels specs

--- src/entities/ground-line/GroundLineLevels.ts
+++ src/entities/ground-line/GroundLineLevels.ts
@@ -2,7 +2,7 @@ import { IGroundLineLevelCharacteristics } from "./IGroundLineLevelCharacteristi
 
 const GroundLineLevels: Record<number, IGroundLineLevelCharacteristics> = {
     1: {
-        hp: 100, 
+        hp: 500, 
         shield: 0, 
         damage: 0, 
         regenerationRate: 0, 
