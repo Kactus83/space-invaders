0930941de87de924ac2269086bed2bd5cc2c0203
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-03-28 07:20:35 +0100

    Add invader waves

--- src/game-services/invader-wave/waves/wavesConfig.ts
+++ src/game-services/invader-wave/waves/wavesConfig.ts
@@ -6,7 +6,7 @@ export const waveConfigs: WaveConfig[] = [
         delay: 10, // Première vague après 10 secondes
         invaders: [
             { type: InvaderType.Basic, count: 5, row: 1 },
-            { type: InvaderType.Light, count: 5, row: 2 },
+            { type: InvaderType.Basic, count: 5, row: 2 },
         ],
     },
     {
@@ -35,6 +35,41 @@ export const waveConfigs: WaveConfig[] = [
         invaders: [
             { type: InvaderType.Strong, count: 5, row: 1 },
             { type: InvaderType.Strong, count: 5, row: 2 },
+        ],
+    },
+    {
+        delay: 45, // Seconde vague après 20 secondes
+        invaders: [
+            { type: InvaderType.Basic, count: 5, row: 1 },
+            { type: InvaderType.Basic, count: 5, row: 2 },
+        ],
+    },
+    {
+        delay: 28, // Seconde vague après 20 secondes
+        invaders: [
+            { type: InvaderType.Basic, count: 5, row: 1 },
+            { type: InvaderType.Light, count: 5, row: 2 },
+        ],
+    },
+    {
+        delay: 30, // Seconde vague après 20 secondes
+        invaders: [
+            { type: InvaderType.Light, count: 5, row: 1 },
+            { type: InvaderType.Light, count: 5, row: 2 },
+        ],
+    },
+    {
+        delay: 35, // Seconde vague après 20 secondes
+        invaders: [
+            { type: InvaderType.Strong, count: 5, row: 1 },
+            { type: InvaderType.Strong, count: 5, row: 2 },
+        ],
+    },
+    {
+        delay: 10, // Seconde vague après 20 secondes
+        invaders: [
+            { type: InvaderType.Basic, count: 5, row: 1 },
+            { type: InvaderType.Basic, count: 5, row: 2 },
         ],
     },
     // Ajoutez d'autres configurations de vague comme nécessaire
