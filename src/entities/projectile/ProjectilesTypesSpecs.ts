09be0c5cc842afff5335ea9febe26d2c938ad790
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-03-29 07:37:29 +0100

    Add new projectiles

--- src/entities/projectile/ProjectilesTypesSpecs.ts
+++ src/entities/projectile/ProjectilesTypesSpecs.ts
@@ -5,13 +5,22 @@ export const ProjectileSpecs: Record<ProjectileType, IProjectileCharacteristics>
     [ProjectileType.None]: {
         speed: 0, damage: 0, hp: 0, shield: 0, regenerationRate: 0, projectileType: ProjectileType.None
     },
+    [ProjectileType.Light]: {
+        speed: 200, damage: 1, hp: 1, shield: 0, regenerationRate: 0, projectileType: ProjectileType.Light
+    },
     [ProjectileType.Basic]: {
-        speed: 250, damage: 1, hp: 1, shield: 0, regenerationRate: 0, projectileType: ProjectileType.Basic
+        speed: 250, damage: 2, hp: 1, shield: 0, regenerationRate: 0, projectileType: ProjectileType.Basic
+    },
+    [ProjectileType.Medium]: {
+        speed: 300, damage: 3, hp: 2, shield: 0, regenerationRate: 0, projectileType: ProjectileType.Medium
     },
     [ProjectileType.Advanced]: {
-        speed: 350, damage: 2, hp: 2, shield: 0, regenerationRate: 0, projectileType: ProjectileType.Advanced
+        speed: 350, damage: 5, hp: 2, shield: 0, regenerationRate: 0, projectileType: ProjectileType.Advanced
+    },
+    [ProjectileType.Heavy]: {
+        speed: 400, damage: 7, hp: 3, shield: 0, regenerationRate: 0, projectileType: ProjectileType.Heavy
     },
     [ProjectileType.Ultimate]: {
-        speed: 500, damage: 4, hp: 3, shield: 0, regenerationRate: 0, projectileType: ProjectileType.Ultimate
+        speed: 500, damage: 10, hp: 5, shield: 1, regenerationRate: 0, projectileType: ProjectileType.Ultimate
     },
 };
--- src/themes/lib/alternative-theme/designs/ProjectileDesigns.ts
+++ src/themes/lib/alternative-theme/designs/ProjectileDesigns.ts
@@ -1,8 +1,11 @@
 import { ProjectileDesign } from '../../../models/ProjectileDesign';
 
 export const projectileDesigns: Record<string, ProjectileDesign> = {
-    "None": new ProjectileDesign("None", "no need to have a svg math", 0, 0),
-    "Basic": new ProjectileDesign("Basic", "assets/themes/default/projectiles/basic.svg", 5, 20),
-    "Advanced": new ProjectileDesign("Advanced", "assets/themes/default/projectiles/advanced.svg", 7, 25),
-    "Ultimate": new ProjectileDesign("Ultimate", "assets/themes/default/projectiles/ultimate.svg", 10, 30),
+    "None": new ProjectileDesign("None", "no need to have a svg path", 0, 0),
+    "Light": new ProjectileDesign("Light", "assets/themes/alternative/projectiles/light.svg", 5, 15),
+    "Basic": new ProjectileDesign("Basic", "assets/themes/alternative/projectiles/basic.svg", 5, 20),
+    "Medium": new ProjectileDesign("Medium", "assets/themes/alternative/projectiles/medium.svg", 6, 20),
+    "Advanced": new ProjectileDesign("Advanced", "assets/themes/alternative/projectiles/advanced.svg", 7, 25),
+    "Heavy": new ProjectileDesign("Heavy", "assets/themes/alternative/projectiles/heavy.svg", 7, 25),
+    "Ultimate": new ProjectileDesign("Ultimate", "assets/themes/alternative/projectiles/ultimate.svg", 10, 30),
 };
