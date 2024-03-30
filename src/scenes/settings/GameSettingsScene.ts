06619025d5ff8110ac958ce5d27000e20a1167c1
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-03-30 19:38:16 +0100

    Add theme management in settings

--- src/scenes/settings/GameSettingsScene.ts
+++ src/scenes/settings/GameSettingsScene.ts
@@ -4,16 +4,26 @@ import { Menu } from "../../ui/menu/Menu";
 import { SceneIds } from "../../core/scene-manager/types/SceneIds";
 import { SceneManager } from "../../core/scene-manager/SceneManager";
 import { AppConfig } from "../../core/config/AppConfig";
+import { ThemeManager } from "../../themes/services/ThemeManager";
 
 export class GameSettingsScene implements IScene {
     private menu: Menu;
 
     async initialize(): Promise<void> {
+        // Bind des fonctions pour garder le contexte
         this.toggleGodMode = this.toggleGodMode.bind(this);
+        this.changeTheme = this.changeTheme.bind(this);
         this.onBack = this.onBack.bind(this);
 
-        const buttonNames = ['God Mode: ' + (AppConfig.getInstance().god_Mode ? 'On' : 'Off'), 'Back'];
-        const buttonActions = [this.toggleGodMode, this.onBack];
+        const themeManager = ThemeManager.getInstance();
+        const currentThemeName = themeManager.getCurrentThemeName();
+
+        const buttonNames = [
+            'God Mode: ' + (AppConfig.getInstance().god_Mode ? 'On' : 'Off'),
+            `Theme: ${currentThemeName}`, // Affiche le nom du thème actuel
+            'Back'
+        ];
+        const buttonActions = [this.toggleGodMode, this.changeTheme, this.onBack];
 
         this.menu = new Menu(buttonNames, buttonActions);
     }
@@ -36,6 +46,13 @@ export class GameSettingsScene implements IScene {
         // Mise à jour du texte du bouton pour le mode Dieu
         this.menu.updateButton(0, 'God Mode: ' + (config.god_Mode ? 'On' : 'Off'));
         console.log('God Mode: ' + (config.god_Mode ? 'On' : 'Off'));
+    }
+
+    private changeTheme(): void {
+        const themeManager = ThemeManager.getInstance();
+        themeManager.toggleTheme(); // Supposons que cette méthode bascule entre les thèmes
+        const newThemeName = themeManager.getCurrentThemeName(); // Récupère le nom du nouveau thème
+        this.menu.updateButton(1, `Theme: ${newThemeName}`);
     }
 
     private onBack(): void {
--- src/themes/services/ThemeManager.ts
+++ src/themes/services/ThemeManager.ts
@@ -33,4 +33,15 @@ export class ThemeManager {
     public getTheme(): Theme {
         return this.themes.get(this.currentThemeType);
     }
+    
+    // Méthode pour basculer entre les thèmes
+    public toggleTheme(): void {
+        const nextThemeType = this.currentThemeType === ThemeType.Default ? ThemeType.Alternative : ThemeType.Default;
+        this.setTheme(nextThemeType);
+    }
+
+    // Méthode pour obtenir le nom du thème actuel
+    public getCurrentThemeName(): string {
+        return this.currentThemeType === ThemeType.Default ? "Default" : "Alternative";
+    }
 }
