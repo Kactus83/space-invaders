3588b3b284679cb463f8bda5d7cfcf7f2d0e8641
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-03-31 12:48:15 +0200

    Add movement to GameBonus

--- src/entities/bonus/GameBonus.ts
+++ src/entities/bonus/GameBonus.ts
@@ -1,8 +1,11 @@
+import { AppConfig } from "../../core/config/AppConfig";
 import { ThemeManager } from "../../themes/services/ThemeManager";
 import { GameEntity } from "../GameEntity";
+import { GroundLine } from "../ground-line/GroundLine";
 import { BonusType } from "../models/bonus-system/BonusType";
 import { SpeedSystem } from "../models/speed-system/SpeedSystem";
 import { Player } from "../player/Player";
+import { EntityState } from "../types/EntityState";
 import { GameBonusType } from "./GameBonusTypeS";
 import { GameBonusSpecs } from "./GameBonusTypesSpecs";
 
@@ -30,13 +33,30 @@ export class GameBonus extends GameEntity {
     }
 
     public update(deltaTime: number): void {
-        // Logique de mise à jour du bonus
-    }
+        // Vérifier si l'objet fabric existe avant de tenter de le déplacer
+        if (this.fabricObject) {
+            // Convertir deltaTime de millisecondes à secondes pour le calcul de la vitesse
+            const deltaTimeInSeconds = deltaTime / 1000;
+    
+            // Calculer le déplacement basé sur la vitesse du bonus
+            const movement = this.speedSystem.moveSpeed * deltaTimeInSeconds;
+    
+            // Mettre à jour la position verticale du bonus
+            this.fabricObject.top += movement;
+    
+            // Vérifier si le bonus est sorti du bas du canvas et le marquer pour suppression si nécessaire
+            if (this.fabricObject.top > AppConfig.getInstance().canvasHeight) {
+                this.state = EntityState.ToBeRemoved;
+            }
+        }
+    }   
 
     onCollisionWith(entity: GameEntity): void {
         if (entity instanceof Player) {
-            // Appliquer l'effet du bonus au joueur
-            // Supprimer le bonus de la scène
+            // Appliquer le bonus au joueur
+        }
+        if (entity instanceof GroundLine) {
+            this.state = EntityState.ToBeRemoved;
         }
     }
 
