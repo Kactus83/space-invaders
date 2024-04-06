f51a583ca9975f961d7c3a4fd96d4b547e76fa93
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-04-07 00:25:04 +0200

    Fix wall name in theme

--- src/themes/lib/alternative-theme/designs/WallDesigns.ts
+++ src/themes/lib/alternative-theme/designs/WallDesigns.ts
@@ -3,7 +3,7 @@ import { WallDesign } from '../../../models/WallDesign';
 
 export const wallDesigns: Record<WallType, WallDesign> = {
     [WallType.None]: new WallDesign("None", "no design needed", 10, 10),
-    [WallType.Light]: new WallDesign("Damaged", "assets/themes/default/walls/damaged.svg", 10, 10),
+    [WallType.Light]: new WallDesign("Damaged", "assets/themes/default/walls/light.svg", 10, 10),
     [WallType.Basic]: new WallDesign("Basic", "assets/themes/default/walls/basic.svg", 10, 10),
     [WallType.Strong]: new WallDesign("Strong", "assets/themes/default/walls/strong.svg", 10, 10),
     [WallType.Invincible]: new WallDesign("Invincible", "assets/themes/default/walls/invincible.svg", 10, 10),
--- src/themes/lib/default-theme/designs/WallDesigns.ts
+++ src/themes/lib/default-theme/designs/WallDesigns.ts
@@ -6,7 +6,7 @@ const wallSize = AppConfig.getInstance().wall_Size;
 
 export const wallDesigns: Record<WallType, WallDesign> = {
     [WallType.None]: new WallDesign("None", "no design needed", wallSize, wallSize),
-    [WallType.Light]: new WallDesign("Damaged", "assets/themes/default/walls/damaged.svg", wallSize, wallSize),
+    [WallType.Light]: new WallDesign("Damaged", "assets/themes/default/walls/light.svg", wallSize, wallSize),
     [WallType.Basic]: new WallDesign("Basic", "assets/themes/default/walls/basic.svg", wallSize, wallSize),
     [WallType.Strong]: new WallDesign("Strong", "assets/themes/default/walls/strong.svg", wallSize, wallSize),
     [WallType.Invincible]: new WallDesign("Invincible", "assets/themes/default/walls/invincible.svg", wallSize, wallSize),
