0568df98659c7813420dc84be825b0b5dc5f4646
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-04-06 16:19:20 +0200

    Remove acquired skills from shop after purchase

--- src/scenes/shop/ShopSkillsScene.ts
+++ src/scenes/shop/ShopSkillsScene.ts
@@ -1,33 +1,36 @@
 import { IScene } from "../../core/scene-manager/types/IScene";
-import { IRenderable } from "../../core/renderer/Irenderable";
 import { SceneManager } from "../../core/scene-manager/SceneManager";
 import { SceneIds } from "../../core/scene-manager/types/SceneIds";
 import { Menu } from "../../ui/menu/Menu";
 import { SkillLibrary } from "../../entities/models/skill-system/library/SkillLibrary";
-import { ShopService } from "../../game-services/shop/Shopservice";
 import { ExperienceDisplayComponent } from "../../ui/experience-display/ExperienceDisplayComponent";
+import { PlayerProfile } from "../../game-services/player-profile/PlayerProfile";
 import { SkillsIds } from "../../entities/models/skill-system/types/SkillsIds";
-
+import { IRenderable } from "../../core/renderer/Irenderable";
+import { ShopService } from "../../game-services/shop/Shopservice";
 
 export class ShopSkillsScene implements IScene {
     private menu: Menu;
     private experienceDisplay: ExperienceDisplayComponent;
 
     async initialize(): Promise<void> {
+        const profile = PlayerProfile.getInstance();
         const skills = SkillLibrary.getAllSkills();
+        const ownedSkillsIds = profile.getSkills().getSkillsIds();
 
-        const buttonNames = skills.map(skill => `${skill.name}: ${skill.experiencePointsCost} XP`);
+        const skillsToDisplay = skills.filter(skill => !ownedSkillsIds.includes(skill.id));
+
+        const buttonNames = skillsToDisplay.map(skill => `${skill.name}: ${skill.experiencePointsCost} XP`);
         buttonNames.push('Back to Shop Home');
 
-        const buttonActions = skills.map(skill => () => this.onSkillPurchased(skill.id));
+        const buttonActions = skillsToDisplay.map(skill => () => this.onSkillPurchased(skill.id));
         buttonActions.push(() => SceneManager.getInstance().changeScene(SceneIds.Shop));
 
         this.menu = new Menu(buttonNames, buttonActions);
         this.experienceDisplay = new ExperienceDisplayComponent();
     }
 
-    update(deltaTime: number): void {
-    }
+    update(deltaTime: number): void {}
 
     getDrawableObjects(): IRenderable[] {
         return [this.menu, this.experienceDisplay];
@@ -40,9 +43,9 @@ export class ShopSkillsScene implements IScene {
     private onSkillPurchased(skillId: SkillsIds): void {
         const purchased = ShopService.getInstance().buySkill(skillId);
         if (purchased) {
-            alert(`Skill "${skillId}" acquired!`);
+            SceneManager.getInstance().changeScene(SceneIds.Shop_Skills);
         } else {
-            alert("Not enough experience points or skill does not exist.");
+            alert("Not enough experience points or skill already owned.");
         }
     }
 }
