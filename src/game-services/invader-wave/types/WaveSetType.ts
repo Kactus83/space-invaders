00558085dc5b790a0b5bfd84ccd6319e6debdbf3
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-04-07 14:33:25 +0200

    Add invaders waves to balance player evolution difficulty

--- src/game-services/invader-wave/types/WaveSetType.ts
+++ src/game-services/invader-wave/types/WaveSetType.ts
@@ -2,10 +2,13 @@ export enum WaveSetType {
     Beginner = "Beginner",
     Beginner_2 = "Beginner_2",
     Beginner_3 = "Beginner_3",
+    Beginner_4 = "Beginner_4",
     Intermediate = "Intermediate",
     Intermediate_2 = "Intermediate_2",
     Intermediate_3 = "Intermediate_3",
+    Intermediate_4 = "Intermediate_4",
     Expert = "Expert",
     Expert_2 = "Expert_2",
     Expert_3 = "Expert_3",
+    Expert_4 = "Expert_4",
 }
--- src/game-services/invader-wave/waves/waveSetsConfig.ts
+++ src/game-services/invader-wave/waves/waveSetsConfig.ts
@@ -9,6 +9,9 @@ import { intermediateWaves_2 } from "./library/intermediateWaves_2";
 import { intermediateWaves_3 } from "./library/intermediateWaves_3";
 import { expertWaves_2 } from "./library/expertWaves_2";
 import { expertWaves_3 } from "./library/expertWaves_3";
+import { beginnerWaves_4 } from "./library/beginnerWaves_4";
+import { intermediateWaves_4 } from "./library/intermediateWaves_4";
+import { expertWaves_4 } from "./library/expertWaves_4";
 
 export const waveSetsConfig: WaveSetConfiguration[] = [
   {
@@ -18,42 +21,57 @@ export const waveSetsConfig: WaveSetConfiguration[] = [
   },
   {
     name: WaveSetType.Beginner_2,
-    experienceThreshold: 1000,
+    experienceThreshold: 750,
     waveConfigs: beginnerWaves_2,
   },
   {
     name: WaveSetType.Beginner_3,
-    experienceThreshold: 2500,
+    experienceThreshold: 2000,
     waveConfigs: beginnerWaves_3,
   },
   {
-    name: WaveSetType.Intermediate,
+    name: WaveSetType.Beginner_4,
     experienceThreshold: 3500,
+    waveConfigs: beginnerWaves_4,
+  },
+  {
+    name: WaveSetType.Intermediate,
+    experienceThreshold: 5000,
     waveConfigs: intermediateWaves,
   },
   {
     name: WaveSetType.Intermediate_2,
-    experienceThreshold: 5000,
+    experienceThreshold: 7000,
     waveConfigs: intermediateWaves_2,
   },
   {
     name: WaveSetType.Intermediate_3,
-    experienceThreshold: 7000,
+    experienceThreshold: 9000,
     waveConfigs: intermediateWaves_3,
   },
   {
+    name: WaveSetType.Intermediate_4,
+    experienceThreshold: 12000,
+    waveConfigs: intermediateWaves_4,
+  },
+  {
     name: WaveSetType.Expert,
-    experienceThreshold: 10000,
+    experienceThreshold: 18000,
     waveConfigs: expertWaves,
   },
   {
     name: WaveSetType.Expert_2,
-    experienceThreshold: 13000,
+    experienceThreshold: 24000,
     waveConfigs: expertWaves_2,
   },
   {
     name: WaveSetType.Expert_3,
-    experienceThreshold: 16000,
+    experienceThreshold: 30000,
     waveConfigs: expertWaves_3,
+  },
+  {
+    name: WaveSetType.Expert_4,
+    experienceThreshold: 38000,
+    waveConfigs: expertWaves_4,
   },
 ];
