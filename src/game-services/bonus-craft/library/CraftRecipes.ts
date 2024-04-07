e656295ddae4d1f6061516496f8b9c1430349daa
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-04-07 02:09:26 +0200

    Add craft recipes

--- src/game-services/bonus-craft/library/CraftRecipes.ts
+++ src/game-services/bonus-craft/library/CraftRecipes.ts
@@ -10,5 +10,36 @@ export const craftRecipes: CraftRecipe[] = [
         requiredBonuses: [GameBonusType.Health_Increase_2_Shield_60sec, GameBonusType.Health_Increase_2_Shield_60sec, GameBonusType.Health_Increase_2_Shield_60sec],
         resultBonus: GameBonusType.Health_Increase_5_Shield_60sec,
     },
-    // Plus de recettes ici...
+    {
+        requiredBonuses: [GameBonusType.Weapon_Double_FireRate_5sec, GameBonusType.Weapon_Double_FireRate_5sec, GameBonusType.Weapon_Double_FireRate_5sec],
+        resultBonus: GameBonusType.Weapon_Double_Firerate_10sec,
+    },
+    {
+        requiredBonuses: [GameBonusType.Weapon_Double_Firerate_10sec, GameBonusType.Weapon_Double_Firerate_10sec, GameBonusType.Weapon_Double_Firerate_10sec],
+        resultBonus: GameBonusType.Weapon_Double_Firerate_20sec,
+    },
+    {
+        requiredBonuses: [GameBonusType.Weapon_Double_Firerate_20sec, GameBonusType.Weapon_Double_Firerate_20sec, GameBonusType.Weapon_Double_Firerate_20sec],
+        resultBonus: GameBonusType.Weapon_Double_Firerate_30sec,
+    },
+    {
+        requiredBonuses: [GameBonusType.Speed_Double_Speed_30sec, GameBonusType.Speed_Double_Speed_30sec, GameBonusType.Speed_Double_Speed_30sec],
+        resultBonus: GameBonusType.Speed_Tripple_Speed_30sec,
+    },
+    {
+        requiredBonuses: [GameBonusType.Speed_Tripple_Speed_30sec, GameBonusType.Speed_Tripple_Speed_30sec, GameBonusType.Speed_Tripple_Speed_30sec],
+        resultBonus: GameBonusType.Speed_Increase_5_Speed_60sec,
+    },
+    {
+        requiredBonuses: [GameBonusType.Speed_Increase_5_Speed_60sec, GameBonusType.Speed_Increase_5_Speed_60sec, GameBonusType.Speed_Increase_5_Speed_60sec],
+        resultBonus: GameBonusType.Speed_Increase_10_Speed_60sec,
+    },
+    {
+        requiredBonuses: [GameBonusType.Speed_Increase_10_Speed_60sec, GameBonusType.Speed_Increase_10_Speed_60sec, GameBonusType.Speed_Increase_10_Speed_60sec],
+        resultBonus: GameBonusType.Speed_Increase_20_Speed_60sec,
+    },
+    {
+        requiredBonuses: [GameBonusType.Experience_Increase_500_Score, GameBonusType.Experience_Increase_500_Score, GameBonusType.Experience_Increase_500_Score],
+        resultBonus: GameBonusType.Experience_Increase_1000_Score,
+    },
 ];
