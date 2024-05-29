bc117beb5c321c0cf399b098575f96771f658ad1
Kactus83 <florian.morena@gmail.com>
2024-05-29 14:46:32 +0200

    WIPonbonuses conversionand restauratio from backend data

--- frontend/src/@game/services/player-profile/inventory/BonusSystemInventory.ts
+++ frontend/src/@game/services/player-profile/inventory/BonusSystemInventory.ts
@@ -1,42 +1,13 @@
-import { GameBonusType } from "../../../entities/bonus/GameBonusTypes";
 import { SystemBonus } from "../../../entities/models/bonus-system/system-bonus/SystemBonus";
-import { ExperienceBonus } from "../../../entities/models/experience-system/bonus/ExperienceBonus";
-import { ExperienceBonusEffect } from "../../../entities/models/experience-system/bonus/ExperienceBonusEffect";
-import { HealthBonus } from "../../../entities/models/health-system/bonus/HealthBonus";
-import { HealthBonusEffect } from "../../../entities/models/health-system/bonus/HealthBonusEffect";
-import { SpeedBonus } from "../../../entities/models/speed-system/bonus/SpeedBonus";
-import { SpeedBonusEffect } from "../../../entities/models/speed-system/bonus/SpeedBonusEffect";
-import { WeaponBonus } from "../../../entities/models/weapon-system/bonus/WeaponBonus";
-import { WeaponBonusEffect } from "../../../entities/models/weapon-system/bonus/WeaponBonusEffect";
 
 export class BonusSystemInventory {
     private bonuses: SystemBonus[] = [];
 
-
-    restoreBonuses(data: any[]): void {
-        
-        if (!data) {
-            console.error("No bonus data available to restore.");
-            return;
-        }
-
-        this.bonuses = data.map(item => {
-            switch (item.type) {
-                case 'Health':
-                    return new HealthBonus(item.effect as HealthBonusEffect);
-                case 'Speed':
-                    return new SpeedBonus(item.effect as SpeedBonusEffect);
-                case 'Experience':
-                    return new ExperienceBonus(item.effect as ExperienceBonusEffect);
-                case 'Weapon':
-                    return new WeaponBonus(item.effect as WeaponBonusEffect);
-                default:
-                    throw new Error(`Unknown bonus type: ${item.type}`);
-            }
-        });
+    setBonuses(bonuses: SystemBonus[]): void {
+        this.bonuses = bonuses;
     }
 
-    addBonus(bonus: SystemBonus) {
+    addBonus(bonus: SystemBonus): void {
         this.bonuses.push(bonus);
     }
 
