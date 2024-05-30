71f50f6be58684039d063b5ab84baaf0a0bb71da
Kactus83 <florian.morena@gmail.com>
2024-05-30 11:09:06 +0200

    Quick fix in Backend GameEngine

--- frontend/src/@game/GameEngine.ts
+++ frontend/src/@game/GameEngine.ts
@@ -9,20 +9,29 @@ export class GameEngine {
     private lastFrameTimeMs: number = 0;
     private scene: GamePlayScene;
     private renderer: Renderer;
+    private intervalId: NodeJS.Timeout;
 
     constructor() {
         this.renderer = new Renderer();
     }
 
+    /**
+     * Initializes and starts the game engine.
+     * 
+     * @param canvasElement The HTML canvas element to be used for rendering the game.
+     */
     public async start(canvasElement: HTMLCanvasElement): Promise<void> {
         this.renderer.initialize(canvasElement);
         this.scene = new GamePlayScene();
         await this.scene.initialize();
-        this.gameLoop();
+        this.startGameLoop();
     }
 
-    private gameLoop(): void {
-        setInterval(() => {
+    /**
+     * Starts the game loop using setInterval for consistent frame updates.
+     */
+    private startGameLoop(): void {
+        this.intervalId = setInterval(() => {
             const deltaTime = Date.now() - this.lastFrameTimeMs;
             this.lastFrameTimeMs = Date.now();
             this.scene.update(deltaTime);
@@ -30,18 +39,41 @@ export class GameEngine {
         }, 1000 / 60); // Run at 60 FPS
     }
 
+    /**
+     * Renders the current state of the game scene.
+     */
     private render(): void {
         const objectsToDraw = this.scene.getDrawableObjects();
         this.renderer.draw(objectsToDraw);
     }
 
+    /**
+     * Synchronizes the game state with the provided state.
+     * 
+     * @param gameState The state to synchronize with.
+     */
     public sync(gameState: any): void {
         this.scene.sync(gameState);
     }
 
+    /**
+     * Initializes the player profile with data from the backend.
+     * 
+     * @param profile The player profile data from the backend.
+     */
     public initializePlayerProfile(profile: FullPlayerProfile): void {
         const playerProfileData: PlayerProfileData = PlayerDataService.getInstance().convertBackendProfile(profile);
         PlayerDataService.getInstance().setProfile(playerProfileData);
         PlayerProfile.getInstance();
     }
+
+    /**
+     * Stops the game loop.
+     */
+    public stop(): void {
+        if (this.intervalId) {
+            clearInterval(this.intervalId);
+        }
+    }
 }
+
